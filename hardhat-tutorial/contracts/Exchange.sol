2ddc42819d66f46e672ece48299a352b6afd325b
Payden <69829508+Payden1224@users.noreply.github.com>
2023-05-20 15:35:16 -0500

    first commit

---
+++ hardhat-tutorial/contracts/Exchange.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Exchange is ERC20 {
+
+    address public cryptoDevTokenAddress;
+
+    // Exchange is inheriting ERC20, because our exchange would keep track of Crypto Dev LP tokens
+    constructor(address _CryptoDevtoken) ERC20("CryptoDev LP Token", "CDLP") {
+        require(_CryptoDevtoken != address(0), "Token address passed is a null address");
+        cryptoDevTokenAddress = _CryptoDevtoken;
+    }
+
+    /**
+* @dev Returns the amount of `Crypto Dev Tokens` held by the contract
+*/
+
+function getReserve() public view returns (uint) {
+    return ERC20(cryptoDevTokenAddress).balanceOf(address(this));
+}
+}
